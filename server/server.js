94056217d67d1e0ac567c0b15c0e850ac267d822
Jordi Yaputra <Jordiyaputra@gmail.com>
2019-08-19 04:28:54 +0700

    Changed port to process.env.PORT instead of 7000

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 const clientPath = `${__dirname}/../client`;
 const libPath = `${__dirname}/../../libraries`;
